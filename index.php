6dc1f1009665bb2b800da4df765171bc861efd0b
Ekamura <81437698+Ekamura@users.noreply.github.com>
2021-04-07 18:02:47 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,272 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Cardo:400i|Open+Sans:700|Raleway:300,400,600,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="css/stat.css">
+    <link rel="stylesheet" href="css/logo.css">
+    <link rel="stylesheet" href="css/historyPlayer.css">
+    <title>Document</title>
+</head>
+<body>
+<header class="header" id="header">
+    <div class="container">
+        <div class="header__inner">
+            <div class="header__logo">
+                <a href="index.php"> <label><img src="img/Снимок4.PNG" alt=""></label></a>
+            </div>
+
+<!--            <nav class="nav" id="nav">-->
+<!--                <a class="nav__link" href="#" data-scroll="#features">История</a>-->
+<!--                <a class="nav__link" href="#" data-scroll="#works">Правила</a>-->
+<!--                <a class="nav__link" href="#" data-scroll="#team">Тренировки</a>-->
+<!--                <a class="nav__link" href="#" data-scroll="#reviews">Спортивный инвентарь</a>-->
+<!--            </nav>-->
+<!--            <div class="navbar">-->
+<!--                <div class="dropdown">-->
+<!--                    <button class="dropbtn" onclick="myFunction()">История-->
+<!--                        <i class="fa fa-caret-down"></i>-->
+<!--                    </button>-->
+<!--                    <div class="dropdown-content" id="myDropdown">-->
+<!--                        <a href="#">Link 3</a>-->
+<!--                        <a href="#">Link 21</a>-->
+<!--                        <a href="#">Link 123</a>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--                <div class="dropdown">-->
+<!--                    <button class="dropbtn" onclick="myFunction()">Правила-->
+<!--                        <i class="fa fa-caret-down"></i>-->
+<!--                    </button>-->
+<!--                    <div class="dropdown-content" id="myDropdown">-->
+<!--                        <a href="#">Link 2</a>-->
+<!--                        <a href="#">Link 4</a>-->
+<!--                        <a href="#">Link 5</a>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--                <div class="dropdown">-->
+<!--                    <button class="dropbtn" onclick="myFunction()">Тренировки-->
+<!--                        <i class="fa fa-caret-down"></i>-->
+<!--                    </button>-->
+<!--                    <div class="dropdown-content" id="myDropdown">-->
+<!--                        <a href="#">Link 1</a>-->
+<!--                        <a href="#">Link 2</a>-->
+<!--                        <a href="#">Link 3</a>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--                <div class="dropdown">-->
+<!--                    <button class="dropbtn" onclick="myFunction()">Спортивный инвентарь-->
+<!--                        <i class="fa fa-caret-down"></i>-->
+<!--                    </button>-->
+<!--                    <div class="dropdown-content" id="myDropdown">-->
+<!--                        <a href="#">Link 1</a>-->
+<!--                        <a href="#">Link 2</a>-->
+<!--                        <a href="#">Link 3</a>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--            <div class="navbar">-->
+<!--            <ul class="menu">-->
+<!--                <li><a class="nav__link" href=#>История</a>-->
+<!--                    <ul class="submenu">-->
+<!--                        <li><a class="nav__link" href=#>Самые популярные</a></li>-->
+<!--                        <li><a class="nav__link" href=#>Самые новые</a></li>-->
+<!--                        <li><a class="nav__link" href=#>Показать все</a></li>-->
+<!--                    </ul></li>-->
+<!--                <li><a class="nav__link" href=#>Правила</a>-->
+<!--                    <ul class="submenu">-->
+<!--                        <li><a class="nav__link" href=#>Самые популярные</a></li>-->
+<!--                        <li><a class="nav__link" href=#>Самые новые</a></li>-->
+<!--                        <li><a class="nav__link" href=#>Показать все</a></li>-->
+<!--                    </ul></li>-->
+<!--                <li><a class="nav__link" href=#>Тренировки</a>-->
+<!--                    <ul class="submenu">-->
+<!--                        <li><a class="nav__link" href=#>Самые популярные</a></li>-->
+<!--                        <li><a class="nav__link" href=#>Самые новые</a></li>-->
+<!--                        <li><a class="nav__link" href=#>Показать все</a></li>-->
+<!--                    </ul></li>-->
+<!--                <li><a class="nav__link" href=#>Спортивный инвентарь</a>-->
+<!--                    <ul class="submenu">-->
+<!--                        <li><a class="nav__link" href=#>Самые популярные</a></li>-->
+<!--                        <li><a class="nav__link" href=#>Самые новые</a></li>-->
+<!--                        <li><a class="nav__link" href=#>Показать все</a></li>-->
+<!--                    </ul></li>-->
+<!--            </ul>-->
+<!--            </div>-->
+            <nav>
+            <ul class="topmenu">
+                <li><a href="">История</a>
+                    <ul class="submenu">
+                        <li><a class="nav__link" href=#>Самые популярные</a></li>
+                        <li><a class="nav__link" href=#>Самые новые</a></li>
+                        <li><a class="nav__link" href=#>Показать все</a></li>
+                    </ul></li>
+                <li><a href="">Правила</a>
+                    <ul class="submenu">
+                        <li><a class="nav__link" href=#>Самые популярные</a></li>
+                        <li><a class="nav__link" href=#>Самые новые</a></li>
+                        <li><a class="nav__link" href=#>Показать все</a></li>
+                    </ul></li>
+                <li><a href="">Тренировки</a>
+                    <ul class="submenu">
+                        <li><a class="nav__link" href=#>Самые популярные</a></li>
+                        <li><a class="nav__link" href=#>Самые новые</a></li>
+                        <li><a class="nav__link" href=#>Показать все</a></li>
+                    </ul></li>
+                <li><a href="">Спортивный инвентарь</a><ul class="submenu">
+                        <li><a class="nav__link" href=#>Самые популярные</a></li>
+                        <li><a class="nav__link" href=#>Самые новые</a></li>
+                        <li><a class="nav__link" href=#>Показать все</a></li>
+                    </ul></li>
+            </ul>
+            </nav>
+            <button class="burger" type="button" id="navToggle">
+                <span class="burger__item">Меню</span>
+            </button>
+
+        </div>
+    </div>
+</header>
+<section class="section  section--devices">
+    <div class="container">
+
+        <div class="section__header">
+            <h3 class="section__suptitle">For all devices</h3>
+            <h2 class="section__title">Unique design</h2>
+        </div>
+
+        <div class="devices">
+            <img class="devices__item" src="img/historyplayers.png" alt="">
+            <img class="devices__item  devices__item--iphone" src="img/kubok.png" alt="">
+        </div>
+
+    </div>
+</section>
+<!-- /.reviews -->
+<!-- Features -->
+
+<div class="statistics">
+    <div class="container">
+
+        <div class="stat">
+            <div class="stat__item">
+                <div class="stat__count">42</div>
+                <div class="stat__text">Web Design Projects</div>
+            </div>
+            <div class="stat__item">
+                <div class="stat__count">123</div>
+                <div class="stat__text">happy client</div>
+            </div>
+            <div class="stat__item">
+                <div class="stat__count">15</div>
+                <div class="stat__text">award winner</div>
+            </div>
+            <div class="stat__item">
+                <div class="stat__count">99</div>
+                <div class="stat__text">cup of coffee</div>
+            </div>
+            <div class="stat__item">
+                <div class="stat__count">24</div>
+                <div class="stat__text">members</div>
+            </div>
+        </div>
+
+    </div>
+</div>
+<div class="reviews" id="reviews">
+    <div class="container">
+        <div class="reviews__slider" id="reviewsSlider">
+            <div class="slick-item">
+                <div class="reviews__item">
+                    <div class="reviews__photo">
+                        <a href="#">
+                            <img class="reviews__img" src="img/история.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="reviews__content">
+                        <div class="reviews__text">“Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec sed odio dui. Aenean eu leo quam...”</div>
+                        <div class="reviews__author">Susan Sims, Interaction Designer at XYZ</div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="slick-item">
+                <div class="reviews__item">
+                    <div class="reviews__photo">
+                        <a href="#">
+                            <img class="reviews__img" src="img/правила.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="reviews__content">
+                        <div class="reviews__text">“Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat iure sed placeat laborum, labore explicabo impedit qui! Laudantium, unde voluptates sit animi tenetur veniam dolorem corrupti magnam, ipsum ab, recusandae!”</div>
+                        <div class="reviews__author">Susan Sims, Interaction Designer at XYZ</div>
+                    </div>
+                </div>
+            </div>
+            <div class="slick-item">
+                <div class="reviews__item">
+                    <div class="reviews__photo">
+                        <a href="#">
+                            <img class="reviews__img" src="img/streetball.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="reviews__content">
+                        <div class="reviews__text">“Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat iure sed placeat laborum, labore explicabo impedit qui! Laudantium, unde voluptates sit animi tenetur veniam dolorem corrupti magnam, ipsum ab, recusandae!”</div>
+                        <div class="reviews__author">Susan Sims, Interaction Designer at XYZ</div>
+                    </div>
+                </div>
+            </div>
+            <div class="slick-item">
+                <div class="reviews__item">
+                    <div class="reviews__photo">
+                        <a href="#">
+                            <img class="reviews__img" src="img/inventory.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="reviews__content">
+                        <div class="reviews__text">“Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat iure sed placeat laborum, labore explicabo impedit qui! Laudantium, unde voluptates sit animi tenetur veniam dolorem corrupti magnam, ipsum ab, recusandae!”</div>
+                        <div class="reviews__author">Susan Sims, Interaction Designer at XYZ</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div><!-- /.container -->
+</div>
+<div class="section  section--gray">
+    <div class="container">
+
+        <div class="logos">
+            <div class="logos__item">
+                <img class="logos__img" src="img/спонсор1.png" alt="">
+            </div>
+            <div class="logos__item">
+                <img class="logos__img" src="img/спонсор2.png" alt="">
+            </div>
+            <div class="logos__item">
+                <img class="logos__img" src="img/спонсор3.png" alt="">
+            </div>
+            <div class="logos__item">
+                <img class="logos__img" src="img/спонсор4.png" alt="">
+            </div>
+            <div class="logos__item">
+                <img class="logos__img" src="img/спонсор5.png" alt="">
+            </div>
+        </div>
+
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<script src="app.js"></script>
+</body>
+</html>
